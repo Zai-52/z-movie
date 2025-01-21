b2d45f738be7bf99dd444e3ecd5402cb33e7de60
Zaw Ye Htet (DAT) <zawyehtet7928@gmail.com>
2025-01-21 16:49:13 +0630

    Add movies in home page.

--- app/page.js
+++ app/page.js
@@ -1,7 +1,38 @@
-import Image from "next/image";
+import Movies from "@/components/Movies";
 
-export default function Home() {
+const token = process.env.TMDB_TOKEN;
+
+async function fetchPopular() {
+  const res = await fetch("https://api.themoviedb.org/3/movie/popular", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return await res.json();
+}
+
+async function fetchTrending() {
+  const res = await fetch("https://api.themoviedb.org/3/tending/movie/day", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return await res.json();
+}
+
+export default async function Home() {
+  const popular = await fetchPopular();
+  const trending = await fetchTrending();
+
   return (
-    <div>Home</div>
+    <div>
+      <h3 className="font-bold border-b mb-4 pb-2">Popular</h3>
+      <Movies movies={popular.results} />
+
+      <h3 className="font-bold border-b mb-4 pb-2">Trending</h3>
+      <Movies movies={trending.results} />
+    </div>
   );
 }
