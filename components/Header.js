fae2834ca660cfb1d888d170b60ce04e4aa4e650
Zaw Ye Htet (DAT) <zawyehtet7928@gmail.com>
2025-01-22 14:35:24 +0630

    added search function.

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,33 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-export default async function headers() {
+export default function header() {
+
+    async function search(formData) {
+        "use server";
+
+        const q = formData.get("q");
+        redirect(`/search?q=${q}`);
+    }
+
     return (
-        <nav className="flex justify-between items-center border rounded
-            rounded border-slate-300 px-4 py-2 my-4
-        ">
+        <nav className="flex justify-between items-center border border-slate-300 rounded px-4 py-2 my-4">
             <h1 className="font-bold text-lg">
                 <Link href="/">Z Movies</Link>
             </h1>
+
+            <form
+                action={search}
+                className="flex gap-2">
+                <Input
+                    type="text"
+                    name="q"
+                    placeholder="Search Movies"
+                />
+                <Button type="submit">Search</Button>
+            </form>
         </nav>
     );
 }
