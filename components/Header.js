a6aadea2f21aa92900d62d563c205e591a669cbb
Zaw Ye Htet (DAT) <zawyehtet7928@gmail.com>
2025-01-22 10:28:46 +0630

    Add genres api.

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default async function headers() {
             rounded border-slate-300 px-4 py-2 my-4
         ">
             <h1 className="font-bold text-lg">
-                <Link href="/">Next Movies</Link>
+                <Link href="/">Z Movies</Link>
             </h1>
         </nav>
     );
